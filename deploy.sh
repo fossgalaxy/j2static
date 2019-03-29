d36141e35125a0666c95a888f9424eef660293f5
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2019-03-29 12:59:50 +0000

    add a reminder as to what libraries are required to build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
+##
+# requires python3-wheel, python3-twine
+##
+
 set -e
 
+# build dist
 rm -rf build/ dist/
 python3 setup.py sdist
 python3 setup.py bdist_wheel
-twine-3 upload dist/*
+
+# use twine (not sure why twine-3 no longer works...)
+python3 -m twine check dist/*
+python3 -m twine upload dist/*
